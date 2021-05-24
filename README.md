0da2066c8454aac7c49f340a330ba3c76c615533
JamesYLiberis <James.Yan@liberis.co.uk>
2021-05-24 10:37:04 +0100

    add multi path logics

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Impendulo
+
+
+**Approach**
+
+ store data into an array from the form.
+
+ 1. a get request that takes an id parameter and it displays a form.  
+ 2. a post method that stores the data in an array.  
+ 3. display a link after the post method is executed.
+
+ 
+**Improvements** 
+
+ - use a DB perhaps MongoDB  
+ - functions can be broken down to more methods for better readability and reusability
+
+**To run the app**
+
+run "npm install"
+run "npm start"
+
+Go to your browser and type 
+
+localhost:3000
+
+
--- multi-path-stories/app.js
+++ multi-path-stories/app.js
@@ -8,7 +8,10 @@ var hbs = require('express-handlebars');
 var cookieParser = require('cookie-parser');
 var routes = require('./routes/index');
 
+
 var app = express();
+
+app.use(cookieParser());
 
 
 // view engine setup
@@ -16,16 +19,13 @@ app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __di
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use('/', routes);
 
@@ -41,24 +41,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-    });
-  }
-  
-  // production error handler
-  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {}
+      error: err
     });
   });
-  
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 
 module.exports = app;
